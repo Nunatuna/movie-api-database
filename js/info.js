73fb343df19527da74b1024372f3c93d85ea5d5e
Nunatuna <aiden.0407@hotmail.com.com>
2025-10-22 17:03:34 +0200

    Initial commit

---
+++ js/info.js
@@ -0,0 +1,11 @@
+// const ACCOUNT_ID = 22351715
+
+export const BASE_URL = "https://api.themoviedb.org/3/movie"
+
+export const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ODk5ZjU3OWUzMjhkZTY3YzQ5ODg0MzViMGNjODg4MSIsIm5iZiI6MTc1OTMzMjI4MC4wMjcsInN1YiI6IjY4ZGQ0N2I4OTAwMWM1ZjA2ZGUxY2U0YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AkKlefcHYIuZZTr8RrJulDKEXDBB1cex7BEaU-wbBzI'
+  }
+};
---
+++ js/now_playing.js
@@ -0,0 +1,32 @@
+import { BASE_URL, options } from "./info.js";
+
+const MAX_MOVIES = 6;
+const fragment = document.createDocumentFragment();
+
+// Fetch once, then slice the first 5 movies
+try {
+  const res = await fetch(`${BASE_URL}/now_playing?language=en-US&page=1`, options);
+  if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
+  const data = await res.json();
+  const movies = data.results.slice(0, MAX_MOVIES);
+
+  movies.forEach(movie => {
+    const movieCard = document.querySelector('#movie-card').content.cloneNode(true);
+
+    movieCard.querySelector('h3').innerText = movie.title;
+
+    const poster = movieCard.querySelector('img');
+    poster.setAttribute('src', `https://image.tmdb.org/t/p/w500${movie.poster_path}`);
+    poster.setAttribute('alt', movie.title);
+
+    movieCard.querySelector('.movie-overview').innerText = movie.overview;
+    movieCard.querySelector('.movie-title').innerText = movie.title;
+    movieCard.querySelector('.movie-release').innerText = movie.release_date;
+
+    fragment.append(movieCard);
+  });
+
+  document.querySelector('#movie-list').append(fragment);
+} catch (err) {
+  console.error("Fetch error:", err);
+}
